a4cb9f4f6e22443db656b9d3da46d7adf2531eff
Pawan Vijay Tayde <pvt1504@gmail.com>
2024-01-04 14:53:23 +0530

    127:Create save Post Feature

---
+++ src/main/java/com/myblog/myblog151/Myblog151Application.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog151;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog151Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog151Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblog/myblog151/Myblog151ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog151;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog151ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
