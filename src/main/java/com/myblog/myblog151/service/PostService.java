eaee1b474518e58003a2a214d446a9810115102b
Pawan Vijay Tayde <pvt1504@gmail.com>
2024-01-06 23:03:10 +0530

    127:Create save Post Feature Entity class added 6th Jan 2024

---
+++ src/main/java/com/myblog/myblog151/service/PostService.java
@@ -0,0 +1,7 @@
+package com.myblog.myblog151.service;
+
+import com.myblog.myblog151.payload.PostDto;
+
+public interface PostService {
+    PostDto createPost(PostDto postDto);
+}
---
+++ src/main/java/com/myblog/myblog151/service/impl/PostServiceImpl.java
@@ -0,0 +1,33 @@
+package com.myblog.myblog151.service.impl;
+
+import com.myblog.myblog151.controller.Post;
+import com.myblog.myblog151.payload.PostDto;
+import com.myblog.myblog151.repository.PostRepository;
+import com.myblog.myblog151.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        Post savePost = postRepository.save(post);
+
+        PostDto dto = new PostDto();
+        dto.setTitle(savePost.getTitle());
+        dto.setDescription(savePost.getDescription());
+        dto.setContent(savePost.getContent());
+        return dto;
+
+    }
+}
